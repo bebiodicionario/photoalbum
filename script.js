335d2c26c0f5399eda0bb4aca37e123c5901da4f
bebiodicionario <97367666+bebiodicionario@users.noreply.github.com>
2023-12-06 19:56:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 from PIL import Image
 import json
+import marked
 
 # Read configuration
-with open("config.txt") as f:
-  config = json.load(f)
+with open("config.md") as f:
+  config_md = f.read()
+  config = marked.parse(config_md)
+
+# Extract configuration data
+album_names = config["album_names"]
+album_paths = config["album_paths"]
+template_paths = config["template_paths"]
+site_title = config["site_title"]
+navigation_links = config["navigation_links"]
+about_bio = config["about_page_content"]
 
 # Process albums
 albums = {}
-for album_name, album_path in zip(config["album_names"], config["album_paths"]):
+for album_name, album_path in zip(album_names, album_paths):
   images, descriptions = process_album("descriptions.txt", album_path)
   albums[album_name] = {
     "name": album_name,
@@ -15,22 +25,21 @@ for album_name, album_path in zip(config["album_names"], config["album_paths"]):
     "descriptions": descriptions,
   }
 
-# Render template
-template_path = config["template_path"] if request_path == "/" else config["about_template_path"]
-template = open(template_path).read()
+# Render templates
+index_template = open(template_paths["index"]).read()
+about_template = open(template_paths["about"]).read()
 
-rendered_content = ""
-if request_path == "/":
-  rendered_albums = []
-  for album in albums.values():
-    album_template = template.copy()
-    for key, value in album.items():
-      album_template = album_template.replace("{{ " + key + " }}", value)
-    rendered_albums.append(album_template)
-  rendered_content = "\n".join(rendered_albums)
-else:
-  rendered_content = template.replace("{{ about_bio }}", config["about_bio"])
+rendered_albums = []
+for album in albums.values():
+  album_template = index_template.copy()
+  for key, value in album.items():
+    album_template = album_template.replace("{{ " + key + " }}", value)
+  rendered_albums.append(album_template)
 
-# Generate HTML and write it to a file
+rendered_about_content = about_template.replace("{{ about_bio }}", about_bio)
+
+# Generate HTML and write it to files
 with open("index.html", "w") as f:
-  f.write(rendered_content)
+  f.write("\n".join(rendered_albums))
+with open("about.html", "w") as f:
+  f.write(rendered_about_content)
